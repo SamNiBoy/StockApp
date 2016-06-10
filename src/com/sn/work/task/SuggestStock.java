67dffd8d2cbcfa6b00ba0c947ef6158726562bcc
Sam Ni <sam.ni@cn.bleum.com>
2016-06-10 10:18:13 +0800

    ST20-037 Refined the disable gz_flg logic.

--- src/com/sn/work/task/SuggestStock.java
+++ src/com/sn/work/task/SuggestStock.java
@@ -374,10 +374,11 @@ public class SuggestStock implements IWork {
 			sql =   " update usrStk u set gz_flg = 0 "
 					+ "where u.sell_mode_flg = 1"
 					+ "  and u.id = '" + stkid + "'"
-					+ "  and exists(select 'x' from "
+					+ "  and (exists(select 'x' from "
                     + "                   (select sum(amount) totamt, buy_flg from tradedtl where stkid = u.id and acntid like 'ACNT%' group by buy_flg) t1, "
                     + "                   (select sum(amount) totamt, buy_flg from tradedtl where stkid = u.id and acntid like 'ACNT%' group by buy_flg) t2 "
-                    + "                   where t1.buy_flg = 1 and t2.buy_flg = 0 and t1.totamt <= t2.totamt) ";
+                    + "                   where (t1.buy_flg = 1 and t2.buy_flg = 0 and t1.totamt <= t2.totamt) or t1.buy_flg is null) "
+                    + "        or not exists (select 'x' from tradedtl where stkid = u.id)) ";
 			log.info(sql);
 			stm = con.createStatement();
 			stm.execute(sql);
