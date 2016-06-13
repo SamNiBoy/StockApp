4ea02ef638fcdd975ca2db7048dde790df65fe58
Sam Ni <sam.ni@cn.bleum.com>
2016-06-13 18:08:22 +0800

    ST-LT003 Added DlyQtyPlused for suggest stock.

--- src/com/sn/trade/strategy/imp/STConstants.java
+++ src/com/sn/trade/strategy/imp/STConstants.java
@@ -37,6 +37,7 @@ public class STConstants {
 	public static final double TOP_INCSTOP_BASE_TRADE_THRESH = 0.06;
 	public static final int MAX_NUM_STOCKS_FOR_TRADE = 10;
 	public static final double QTY_PLUSED_BASE_PCT = 0.6;
+	public static final double DLY_DL_QTY_PLUSED_PCT = 0.8;
 	
 	public static final int DEV_CALCULATE_DAYS = 7;
 	public static final double MAX_STEDEV_VALUE = 0.1;
